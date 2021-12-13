09386092548fdc0859cfb4122ac02ca8201ebc12
ahmed <ahmedhossamm1996@gmail.com>
2021-12-13 17:57:25 +0200

    final

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,18 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/', [PortalController::class, 'index']);
+
+
+
+
+
+Route::post('portal', [portalController::class, 'store']);
+
+Route::get('portal/{id}', [IssueController::class, 'update']);
+
+
+
+Route::get('portal/{id}', [IssueController::class, 'delete']);
